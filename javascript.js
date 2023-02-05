381bfab6218c68b747ad0f084de863bb761cd1c9
emShalaby <medo.dice@gmail.com>
2023-02-05 13:54:34 +0200

    add a round function

--- javascript.js
+++ javascript.js
@@ -5,14 +5,37 @@ const cards = document.querySelectorAll('.card');
 
 //----Functions---
 function get_computer_choice(){
-    const RPS=['Rock','Paper','SCISSORS']
+    const RPS=['Rock','Paper','Scissors']
     const random=Math.floor(Math.random() * RPS.length);
     return RPS[random];
 }
 
+function round(computer,player){
+    
+    
+    const RS='Rock beats scissors!';
+    const RP='Paper beats rock!';
+    const PS='Scissors beats paper!';
+    const draw=`Draw! Both players picked ${player}`;
+    const win='You win!';
+    const lose='You lose!';
+    
+    if (player===computer) return draw;
+
+    else if (player=='Rock' && computer=='Paper') return `${lose} ${RP}`;
+    else if (player=='Rock'&& computer=='Scissors') return `${win} ${RS}`;
+    else if (player=='Paper' && computer=='Rock') return `${win} ${RP} `;
+    else if (player=='Paper' && computer=='Scissors') return `${lose} ${PS}`;
+    else if (player=='Scissors' && computer=='Rock') return `${lose} ${RS}`;
+    else if (player=='Scissors' && computer=='Paper') return `${win} ${PS}`
+    
+
+}
 
 //-----EVENTS-----
 playBtn.addEventListener('click',()=>{
     playBtn.style.display='none';
     game.style.display='flex'
 })
+
+alert(round(computer='Rock',player='Scissors'))
