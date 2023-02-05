c5fb57b0f0cbfa8f1d3db590b12126ec41f8c509
emShalaby <medo.dice@gmail.com>
2023-02-05 05:25:46 +0200

    restore the javascript code that changes display mode

--- javascript.js
+++ javascript.js
@@ -2,3 +2,9 @@ const playBtn=document.querySelector('.play');
 const game=document.querySelector('.game');
 const cards = document.querySelectorAll('.card');
 
+
+playBtn.addEventListener('click',()=>{
+    playBtn.style.display='none';
+    game.style.display="flex";
+
+})
