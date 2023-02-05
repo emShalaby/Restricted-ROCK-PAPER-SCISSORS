f08fc197433152bea7c0b17291d521515d7860c8
emShalaby <medo.dice@gmail.com>
2023-02-05 05:02:43 +0200

    show card stack effect

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 const playBtn=document.querySelector('.play');
 const game=document.querySelector('.game');
+const cards = document.querySelectorAll('.card');
+
+cards.forEach((card, index) =>{
+  card.style.zIndex = index;
+});
 playBtn.addEventListener('click',()=>{
     playBtn.style.display='none';
     game.style.display="flex";
