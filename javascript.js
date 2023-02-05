bdb615129fa1776556187ce75b900a3b0e3726bf
emShalaby <medo.dice@gmail.com>
2023-02-05 13:29:30 +0200

    scene change restored

--- javascript.js
+++ javascript.js
@@ -2,9 +2,8 @@ const playBtn=document.querySelector('.play');
 const game=document.querySelector('.game');
 const cards = document.querySelectorAll('.card');
 
-
+// function that gets a randomchoice
 playBtn.addEventListener('click',()=>{
     playBtn.style.display='none';
-    game.style.display="flex";
-
+    game.style.display='flex'
 })
