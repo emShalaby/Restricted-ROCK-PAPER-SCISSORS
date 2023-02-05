7eaee643ddd4c47f5ed656dacb18b03abf002115
emShalaby <medo.dice@gmail.com>
2023-02-05 13:37:46 +0200

    add a function that gets a random computer choice , add a class to each card

--- javascript.js
+++ javascript.js
@@ -2,7 +2,16 @@ const playBtn=document.querySelector('.play');
 const game=document.querySelector('.game');
 const cards = document.querySelectorAll('.card');
 
-// function that gets a randomchoice
+
+//----Functions---
+function get_computer_choice(){
+    const RPS=['Rock','Paper','SCISSORS']
+    const random=Math.floor(Math.random() * RPS.length);
+    return RPS[random];
+}
+
+
+//-----EVENTS-----
 playBtn.addEventListener('click',()=>{
     playBtn.style.display='none';
     game.style.display='flex'
